d46b9a78f2ef024b70fcd74a2e6abd614a646ba3
Your Name <you@example.com>
2023-06-08 20:28:06 -0500

    key add

--- app/proyectos/page.js
+++ app/proyectos/page.js
@@ -9,8 +9,7 @@ export default async function Contacto() {
   return (
     <div id="stats" className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 p-4">
       {gameList.map(game => (
-
-          <div className="flex flex-row items-center justify-center">
+          <div key={game.id} className="flex flex-row items-center justify-center">
             <div className="p-2">
               <div className="relative">
                 <img
