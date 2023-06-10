6dc458bc04639ab29f50a969d2ec172be73ad93f
Your Name <you@example.com>
2023-06-10 10:43:49 -0500

    screen size

--- app/layout.js
+++ app/layout.js
@@ -10,7 +10,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
-      <body className="min-h-screen shadow-inner">
+      <body className="h-screen shadow-inner">
         <Header />
         {children}
         </body>
