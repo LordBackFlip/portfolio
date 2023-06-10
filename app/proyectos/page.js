945f865c434bb6cedb5c34853ec640a76cacac65
Your Name <you@example.com>
2023-06-10 10:49:48 -0500

    sin imagenes

--- app/proyectos/page.js
+++ app/proyectos/page.js
@@ -12,12 +12,12 @@ export default async function Contacto() {
           <div key={game.id} className="flex flex-row items-center justify-center">
             <div className="p-2">
               <div className="relative">
-                <img
+                {/* <img
                   className="w-52 h-52 rounded-lg shadow-md bg-black/20 to-white/10"
                   src={game.icon}
                   alt={game.game_name}
                   loading="lazy"
-                />
+                /> */}
                 <a href="#" className="absolute inline-flex space-x-2 items-center text-center bottom-2 right-2">
                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z" />
