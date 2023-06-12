753fa46615368e79559dc366c5532c387242c780
Your Name <you@example.com>
2023-06-12 15:08:55 -0500

    new method

--- app/servicios/page.js
+++ app/servicios/page.js
@@ -1,16 +1,43 @@
-const fetchGames = () => {
-  return fetch('https://casino-api.starsolpty.com:8443/star-game-orchestrator/site/games?user_id=992722337__DivisionMalawi__MWK&page=1&size=10&name=')
+'use client'
+import React, { useState, useEffect } from 'react';
+import InfiniteScroll from 'react-infinite-scroll-component';
+
+const fetchGames = (page) => {
+  return fetch(`https://casino-api.starsolpty.com:8443/star-game-orchestrator/site/games?user_id=992722337__DivisionMalawi__MWK&page=${page}&size=10&name=`)
     .then(res => res.json())
 }
 
-export default async function Contacto() {
-  const games = await fetchGames()
-  const gameList = games.data.list;
+export default function Contacto() {
+  const [games, setGames] = useState([]);
+  const [page, setPage] = useState(1);
+  const [hasMore, setHasMore] = useState(true);
+  const [initialPageLoaded, setInitialPageLoaded] = useState(false); // Nuevo estado para controlar si se cargó la primera página
+
+  useEffect(() => {
+    fetchGames(1).then(data => {
+      setGames(data.data.list);
+      setPage(2);
+      setInitialPageLoaded(true); // Marcar la primera página como cargada
+    });
+  }, []);
+
+  const loadMore = () => {
+    fetchGames(page).then(data => {
+      if (data.data.list.length === 0) {
+        setHasMore(false);
+        return;
+      }
+
+      setGames([...games, ...data.data.list]);
+      setPage(page + 1);
+    });
+  }
+
   return (
     <div id="stats" className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 p-4">
-      {gameList.map(game => (
-          <div key={game.id} className="flex flex-row items-center justify-center">
-            <div className="p-2">
+      {initialPageLoaded && games.map(game => (
+        <div key={game.id} className="flex flex-row items-center justify-center">
+          <div className="p-2">
               <div className="relative">
                 <img
                   className="w-52 h-52 rounded-lg shadow-md bg-black/20 to-white/10"
@@ -24,11 +51,23 @@ export default async function Contacto() {
                   </svg>
                 </a>
                 </div>
-                <h4 className="text-base font-bold" style={{ maxWidth: '208px', overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap' }}>{game.game_name}</h4>
+                <h4 className="text-base font-bold" style={{ maxWidth: '140px', overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap' }}>{game.game_name}</h4>
                 <p className="text-gray-500 text-xs">Min. Bet {game.min_bet + game.currency}</p>
               </div>
-            </div>
-      ))}
         </div>
-      )
+      ))}
+      {hasMore && initialPageLoaded && (
+        <InfiniteScroll
+          dataLength={games.length}
+          next={loadMore}
+          hasMore={true}
+          loader={
+              <div className="animate-spin w-6 h-6 border-3 border-current border-t-transparent text-blue-600 rounded-full" role="status" aria-label="loading">
+                <span className="sr-only">Loading...</span>
+              </div>
+          }
+        />
+      )}
+    </div>
+  )
 }
