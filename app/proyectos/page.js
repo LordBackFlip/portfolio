75730e7c8aa9f62962413f116bf937918d2d6948
Your Name <you@example.com>
2023-06-12 14:16:46 -0500

    changed

--- app/proyectos/page.js
+++ app/proyectos/page.js
@@ -1,70 +1,35 @@
 'use client'
-import React, { useState, useEffect } from 'react';
-import InfiniteScroll from 'react-infinite-scroll-component';
-
-const fetchGames = (page) => {
-  return fetch(`https://casino-api.starsolpty.com:8443/star-game-orchestrator/site/games?user_id=992722337__DivisionMalawi__MWK&page=${page}&size=10&name=`)
+const fetchGames = () => {
+  return fetch('https://casino-api.starsolpty.com:8443/star-game-orchestrator/site/games?user_id=992722337__DivisionMalawi__MWK&page=1&size=10&name=')
     .then(res => res.json())
 }
 
-export default function Contacto() {
-  const [games, setGames] = useState([]);
-  const [page, setPage] = useState(2);
-  const [hasMore, setHasMore] = useState(true);
-
-  useEffect(() => {
-    fetchGames(1).then(data => {
-      setGames(data.data.list);
-    });
-  }, []);
-
-  const loadMore = () => {
-    fetchGames(page).then(data => {
-      if (data.data.list.length === 0) {
-        setHasMore(false);
-        return;
-      }
-
-      setGames(prevGames => [...prevGames, ...data.data.list]);
-      setPage(prevPage => prevPage + 1);
-    });
-  }
-
+export default async function Contacto() {
+  const games = await fetchGames()
+  const gameList = games.data.list;
   return (
     <div id="stats" className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 p-4">
-      {games.map(game => (
-        <div key={game.id} className="flex flex-row items-center justify-center">
-          <div className="p-2">
-            <div className="relative">
-              <img
-                className="w-52 h-52 rounded-lg shadow-md bg-black/20 to-white/10"
-                src={game.icon}
-                alt={game.game_name}
-                loading="lazy"
-              />
-              <a href="#" className="absolute inline-flex space-x-2 items-center text-center bottom-2 right-2">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
-                  <path strokeLinecap="round" strokeLinejoin="round" d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z" />
-                </svg>
-              </a>
+      {gameList.map(game => (
+          <div key={game.id} className="flex flex-row items-center justify-center">
+            <div className="p-2">
+              <div className="relative">
+                <img
+                  className="w-52 h-52 rounded-lg shadow-md bg-black/20 to-white/10"
+                  src={game.icon}
+                  alt={game.game_name}
+                  loading="lazy"
+                />
+                <a href="#" className="absolute inline-flex space-x-2 items-center text-center bottom-2 right-2">
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z" />
+                  </svg>
+                </a>
+                </div>
+                <h4 className="text-base font-bold" style={{ maxWidth: '208px', overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap' }}>{game.game_name}</h4>
+                <p className="text-gray-500 text-xs">Min. Bet {game.min_bet + game.currency}</p>
+              </div>
             </div>
-            <h4 className="text-base font-bold" style={{ maxWidth: '100px', overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap' }}>{game.game_name}</h4>
-            <p className="text-gray-500 text-xs">Min. Bet {game.min_bet + game.currency}</p>
-          </div>
-        </div>
       ))}
-      {hasMore && (
-        <InfiniteScroll
-          dataLength={games.length}
-          next={loadMore}
-          hasMore={true}
-          loader={
-            <div className="animate-spin inline-block w-6 h-6 border-[3px] border-current border-t-transparent text-blue-600 rounded-full" role="status" aria-label="loading">
-          <span className="sr-only">Loading...</span>
         </div>
-          }
-        />
-      )}
-    </div>
-  )
+      )
 }
