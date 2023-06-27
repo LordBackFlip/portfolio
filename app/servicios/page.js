c54e18b2f47e2044284c6bac3cb4e99093fa60a4
Your Name <you@example.com>
2023-06-27 14:57:22 -0500

    cambio en la distribucion de los juegos

--- app/servicios/page.js
+++ app/servicios/page.js
@@ -42,7 +42,7 @@ export default function Contacto() {
   }
 
   return (
-    <div id="stats" className="relative grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 p-5">
+    <div id="stats" className="relative grid grid-cols-2 sm:grid-cols-4 lg:grid-cols-5 xl:grid-cols-5 gap-4 p-5">
       {initialPageLoaded && games.map(game => (
         <div key={game.id} className="flex-row items-center justify-center">
           <div className="p-2">
